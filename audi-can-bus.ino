b3b6f4d6587e97e2f7b34d27b4ffaa4486649a61
ivv <v.izgarshev@octaviangaming.com>
2017-10-04 18:55:44 +0300

    some changes

--- audi-can-bus.ino
+++ audi-can-bus.ino
@@ -1,27 +1,26 @@
 #include <mcp_can.h>
 #include <mcp_can_dfs.h>
 #include <SPI.h>
-
-//#include <StandardCplusplus.h> // trololo
+//#include <StandardCplusplus.h>
 
 #define IGNITION_STATE  0x271
 #define IGNITION_PIN    5
 /*
-h271 зажигание, 1 byte, 100ms 
--h10: Fzg. unverschlossen, Schluessel steckt nicht ключ снят
+h271 Ignition state, 1 byte, 100ms 
+-h10: Fzg. unverschlossen, Schluessel steckt nicht Without Key
 -h11: Fzg. unverschlossen, Schluessel steckt in Pos. 0, Zuendung aus 
 -h01: Fzg. unverschlossen, Schluessel steckt in Pos. 1, Zuendung aus 
 -h05: Fzg. unverschlossen, Schluessel steckt in Pos. 2, Zuendung aus 
--h07: Fzg. unverschlossen, Schluessel steckt in Pos. 3, зажигание включено
--h0B: Fzg. unverschlossen, Schluessel steckt in Pos. 4, зажигание включено, стартер крутит 
+-h07: Fzg. unverschlossen, Schluessel steckt in Pos. 3, Ignition ON, Starter stopped
+-h0B: Fzg. unverschlossen, Schluessel steckt in Pos. 4, Ignition ON, Starter rotating
 */
 #define LIGHT_STATE     0x635
 #define LIGHT_PIN       
 /*
-h635 Licht, 3 byte, 100ms 
--h 00 00 00: Licht aus, 58d 100% Dimmung 
--h 64 00 00: Licht aus, 58d 0% Dimmung
--h 64 64 00: Licht an, 58d 0% Dimmung 
+h635 Light, 3 byte, 100ms 
+-h 00 00 00: Light OFF, 58d 100% Dimmung 
+-h 64 00 00: Light OFF, 58d 0% Dimmung
+-h 64 64 00: Light ON, 58d 0% Dimmung 
 ID 635 : Licht Dimmung (noch nicht fertig) 
 */
 #define SPEED           0x351
@@ -35,8 +34,6 @@ h351, Geschwindigkeit, 8 byte, 100ms
 - byte 4/5: Wegstreckenimpuls 
 */
 
-// the cs pin of the version after v1.1 is default to D9
-// v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 10;
 MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
@@ -67,32 +64,32 @@ private:
     unsigned long each_ms;
 };
 
-//struct command
-//{
-//    public :
-//        virtual command(uint8_t header, unsigned long each_ms, MCP_CAN* port) = 0;
-//        ~command();
-//
-//    private:
-//        unsigned long   m_each_ms;
-//        timer_          m_reply_timer;
-//        size_t          m_data_size;
-//        uint8_t         m_header;
-//        MCP_CAN*        m_port;
-//};
-//
-//class light_cmd: public command
-//{
-//  public :
-//      virtual command(uint8_t header, unsigned long each_ms, MCP_CAN* port){};
-//}
-//}
+struct command
+{
+    public :
+        command() {};
+        ~command();
+
+    private:
+        unsigned long   m_each_ms;
+        timer_          m_reply_timer;
+        size_t          m_data_size;
+        uint8_t         m_header;
+        MCP_CAN*        m_port;
+};
+
+class light_cmd: public command
+{
+  public :
+      light_cmd(uint8_t id_cmd, unsigned long each_ms, MCP_CAN* port);
+};
+
 
 void setup()
 {
     Serial.begin(115200);
 
-    while (CAN_OK != CAN.begin(CAN_100KBPS, MCP_16MHz))              // init can bus : baudrate = 500k
+    while (CAN_OK != CAN.begin(CAN_100KBPS, MCP_16MHz))
     {
         Serial.println("CAN BUS Shield init fail");
         Serial.println(" Init CAN BUS Shield again");
@@ -101,32 +98,11 @@ void setup()
     Serial.println("CAN BUS Shield init ok!");
 }
 
-unsigned char buffer_L[3] = {0x64, 0, 0};
-unsigned char buffer_i[1] = {0x7};
+uint8_t buffer_L[3] = {0x64, 0, 0};
+uint8_t buffer_i[1] = {0x7};
 
 void loop()
 {
-    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
-/*    buffer[7] = buffer[7]+1;
-    if(buffer[7] == 100)
-    {
-        buffer[7] = 0;
-        buffer[6] = buffer[6] + 1;
-        
-        if(buffer[6] == 100)
-        {
-            buffer[6] = 0;
-            buffer[5] = buffer[6] + 1;
-        }
-    }
-    for(int i = 0; i<8; i++)
-      {
-        if(buffer[i] < 0x10) Serial.print("0");
-        Serial.print(buffer[i], HEX);
-        Serial.print(" ");
-      };
-           Serial.println("");
-           */
     byte temp = CAN.sendMsgBuf(LIGHT_STATE, 0, 3, buffer_L);
     if( temp == CAN_OK )
         Serial.println("CAN BUS Shield - LIGHT sent OK");
